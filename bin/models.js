cde3ddee6866401ce62e9f5e06bd16f310213fc2
Olin Fernandes <olin.f@live.com>
2019-08-14 00:13:51 +0530

    Updated

---
+++ bin/models.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+function handleOps (err, data, callback) {
+	if(err) callback(err);
+	if(!data) callback(null, {msg: 'Data not found!', data: null});
+	if(data) callback(null, {msg: 'Data Found', data});
+	return;
+}
+
+mongoose.connect('mongodb://172.19.0.4/Portfolio', {useNewUrlParser: true})
+	.then(() => console.log('Connected to mongodb2'))
+	.catch(error => console.log(error));
+
+const User = mongoose.model('User', (new Schema({
+	Name: {type: String, default: null, index: true},
+	Email: {type: String, default: null, index: true},
+	Password: {type: String}
+})))
+
+const models = {
+	User,
+	Ops: {
+		find: (callback, params) => {
+			if(!params){
+				User.find()
+					.then((err, data) => (handleOps(err, data, callback)))
+					.catch(error => callback(error));
+			} else {
+				console.log('TODO set param responses');
+				return callback(null, {msg: 'TODO set param responses in bin/model.js'})
+			}
+		}
+	}
+}
+
+module.exports = models;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { User, Ops } = require('../bin/models');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+  // Ops.find((err, response)=> {
+  // 	if(err) res.status(500).send('Something went wrong!');
+  // 	else res.json(response);
+  // 	return next();
+  // });
 });
 
 module.exports = router;
