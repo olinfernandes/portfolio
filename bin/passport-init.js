3dfc1aac56850bf52324425e2f060472b7d55eb6
Olin Fernandes <olin.f@live.com>
2020-10-22 17:05:21 +0530

    Updated frontend

--- bin/passport-init.js
+++ bin/passport-init.js
@@ -30,7 +30,7 @@ module.exports = (passport) => {
             return done(null, false, {
               message: ' User not found with email ' + email,
             });
-          if (req.user.email === email) {
+          if (req.user.email === email || req.user.role === 'ADMIN') {
             User.findOneAndUpdate(
               { email },
               { password: createHash(password) },
